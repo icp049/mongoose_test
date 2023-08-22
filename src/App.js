84030146409b3193acdcc038f12e4f70b7209b81
icp049 <ianpepe2000@gmail.com>
2023-08-22 00:56:35 -0600

    created database connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import react from "react";
+//mongodb+srv://ianpedeglorio:fscv8d1D1huVP410@cluster0.x3pgn2y.mongodb.net/nodejs?retryWrites=true&w=majority
 
-function App() {
-  return (
-       <div>Hello</div>
-  );
-}
 
-export default App;
+const http = require("http")
+
+//lets create the server
+
+const server = http.createServer((req,res) => {
+  //send response
+
+  res.write("server responding! hello!")
+  res.end();
+})
+
+
+//server listen to port 3000
+
+server.listen((3000), () => {
+  console.log("Listening at port 3000")
+})
+
+//create connection with cloud database
+
+let url = "mongodb+srv://ianpedeglorio:fscv8d1D1huVP410@cluster0.x3pgn2y.mongodb.net/nodejs?retryWrites=true&w=majority";
+
+const mongoose = require("mongoose");
+
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once ('open', function callback () {
+  console.log("database connection success");
+});
